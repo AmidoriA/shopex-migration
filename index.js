0dc2d53c2b0f00cd03fd55b9ae951396c2299f60
Tan <ami.o2sx@gmail.com>
2023-10-11 16:55:08 +0700

    Support Github action

---
+++ index.js
@@ -0,0 +1,23 @@
+const Knex = require('knex');
+const { Client } = require('mysql'); // Or your database driver
+const knexConfig = require('./knexfile').production; // Assuming you're running this in production
+
+exports.handler = async (event, context) => {
+  const knex = Knex(knexConfig);
+  
+  try {
+    await knex.migrate.latest(); // You can also run `rollback`, `currentVersion`, etc.
+    return {
+      statusCode: 200,
+      body: 'Migration successful',
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: 'Migration failed',
+    };
+  } finally {
+    await knex.destroy();
+  }
+};
--- knexfile.js
+++ knexfile.js
@@ -32,18 +32,12 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'mysql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      host: 'PROD_STORE_DB_HOST',
+      user: 'PROD_STORE_DB_USER',
+      password: 'PROD_STORE_DB_PASSWORD',
+      database: 'PROD_STORE_DB_NAME'
     }
   }
 
